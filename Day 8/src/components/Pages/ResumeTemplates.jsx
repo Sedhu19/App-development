67dcfc02af133530002a0bf4cfa63b28805c8858
sedhu <104960685+Sedhu19@users.noreply.github.com>
2023-10-03 19:19:00 +0530

    Add files via upload

---
+++ Day 8/src/components/Pages/ResumeTemplates.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from '../nav/Navbar'
+
+export const ResumeTemplates = () => {
+  return (
+    <div>
+        <div>
+        <NavBar />
+            <h1>resume templates</h1>
+        </div>
+    </div>
+  )
+}
