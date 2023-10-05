87b8e102add5716de9a2035bf98215cbe480f21a
sedhu <104960685+Sedhu19@users.noreply.github.com>
2023-10-05 10:30:17 +0530

    Add files via upload

---
+++ Day 11/src/main/java/com/teamace/sedhu/Service/AuthenticationService.java
@@ -0,0 +1,63 @@
+package com.teamace.sedhu.Service;
+
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.teamace.sedhu.Model.User;
+import com.teamace.sedhu.Model.enumerate.Role;
+import com.teamace.sedhu.Repository.UserRepositorys;
+import com.teamace.sedhu.dto.request.AuthenticationRequest;
+import com.teamace.sedhu.dto.request.RegisterRequest;
+import com.teamace.sedhu.dto.response.AuthenticationResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+	
+	private final UserRepositorys repository ; 
+	 private final PasswordEncoder passwordEncoder;
+	 private final JwtService jwtservice;
+	 private final AuthenticationManager authenticationManager;
+	public AuthenticationResponse register(RegisterRequest request) {
+		// TODO Auto-generated method stub
+	
+			
+	    var user = User.builder()
+	            .username(request.getFirstname())
+	            .email(request.getEmail())
+	            .password(passwordEncoder.encode(request.getPassword()))
+	            .role(Role.USER)
+	            .build();
+	    repository.save(user);
+	    var jwtToken = jwtservice.generateToken(user);
+	    
+		return AuthenticationResponse.builder()
+				.token(jwtToken)
+				.build();
+	}
+
+	public AuthenticationResponse authenticate(AuthenticationRequest request) {
+		authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken(
+				request.getUsername(),
+				request.getPassword()
+				)
+				);
+				var user = repository.findByEmail(request.getUsername())
+						.orElseThrow();
+				 var jwtToken = jwtservice.generateToken(user);
+				    
+					return AuthenticationResponse.builder()
+							.token(jwtToken)
+							.build();
+	}
+	
+	
+	
+
+}
