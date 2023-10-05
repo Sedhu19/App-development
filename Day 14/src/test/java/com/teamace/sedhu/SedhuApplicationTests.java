904e036c39e05f3a388084a125bf9ae010771d97
sedhu <104960685+Sedhu19@users.noreply.github.com>
2023-10-05 12:14:27 +0530

    Add files via upload

---
+++ Day 14/src/test/java/com/teamace/sedhu/SedhuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamace.sedhu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SedhuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
