1bc00982e5e885276058c0bc6b99e1ec6826a1de
sedhu <104960685+Sedhu19@users.noreply.github.com>
2023-10-11 12:28:39 +0530

    Add files via upload

---
+++ Day 15/src/main/java/com/teamace/sedhu/Controller/AuthenticationController.java
@@ -0,0 +1,37 @@
+package com.teamace.sedhu.Controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.teamace.sedhu.Service.AuthenticationService;
+import com.teamace.sedhu.dto.request.AuthenticationRequest;
+import com.teamace.sedhu.dto.request.RegisterRequest;
+import com.teamace.sedhu.dto.response.AuthenticationResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/auth")	
+@RequiredArgsConstructor
+public class AuthenticationController {
+	
+	private final AuthenticationService service;
+	
+	  @PostMapping("/register")
+	  public ResponseEntity<AuthenticationResponse> register(
+	      @RequestBody RegisterRequest request
+	  ) {
+	    return ResponseEntity.ok(service.register(request));
+	  }
+	  @PostMapping("/authenticate")
+	  public ResponseEntity<AuthenticationResponse> authenticate(
+	      @RequestBody AuthenticationRequest request
+	  ) {
+	    return ResponseEntity.ok(service.authenticate(request));
+	  }
+	  
+
+}
