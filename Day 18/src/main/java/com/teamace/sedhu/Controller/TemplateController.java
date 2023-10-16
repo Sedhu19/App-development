8ac05b41f13c7e3650961690c0bad4099ab7ca7d
sedhu <104960685+Sedhu19@users.noreply.github.com>
2023-10-16 14:39:44 +0530

    Add files via upload

---
+++ Day 18/src/main/java/com/teamace/sedhu/Controller/TemplateController.java
@@ -0,0 +1,66 @@
+package com.teamace.sedhu.Controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.teamace.sedhu.Model.Template;
+import com.teamace.sedhu.Service.TemplateService;
+import com.teamace.sedhu.dto.request.RegisterRequest;
+import com.teamace.sedhu.dto.request.TemplateRequest;
+import com.teamace.sedhu.dto.response.AuthenticationResponse;
+import com.teamace.sedhu.dto.response.TemplateResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/temp")	
+@CrossOrigin("*")
+@RequiredArgsConstructor
+public class TemplateController {
+	
+	private final TemplateService templateService;
+	
+	@PostMapping("/register")
+	  public ResponseEntity<String> register(
+	      @RequestBody TemplateRequest request
+	  ) {
+	      boolean isSaved = templateService.registertemp(request);
+	        return isSaved ? ResponseEntity.status(201).body("Template added successfully!")
+	                : ResponseEntity.badRequest().build();
+//	    return ResponseEntity.ok(templateService.register(request));
+	  }
+	
+	@GetMapping("/get")
+	 public ResponseEntity<List<Template>> getAllProducts() {
+        List<Template> productList = templateService.getAllProducts();
+        return !productList.isEmpty() ? ResponseEntity.status(200).body(productList)
+                : ResponseEntity.noContent().build();
+    }
+	
+	  @PutMapping("/edit/{pid}")
+	    public ResponseEntity<String> updateProduct(@RequestBody TemplateRequest request, @PathVariable Long pid) {
+	        boolean isUpdate = templateService.updateProduct(request, pid);
+	        return isUpdate ? ResponseEntity.status(201).body("Update Successfull") 
+	        		: ResponseEntity.notFound().build();
+	    }
+	  
+	  @DeleteMapping("/delete/{pid}")
+	  public ResponseEntity<String> delete(@PathVariable("pid") Long pid)
+		{
+		  boolean isDelete = templateService.delete(pid);
+		  return isDelete ? ResponseEntity.status(201).body("Delete Successfull") 
+	        		: ResponseEntity.notFound().build();
+		}
+	
+
+}
---
+++ Day 18/src/main/java/com/teamace/sedhu/client/FeedbackClient.java
@@ -0,0 +1,25 @@
+package com.teamace.sedhu.client;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.teamace.sedhu.dto.request.FeedbackRequest;
+import com.teamace.sedhu.vo.Feedback;
+
+
+
+
+
+@FeignClient(name = "FEEDBACK", url = "${application.config.feedback-service.url}")
+public interface FeedbackClient {
+
+    @PostMapping("/add")
+    boolean saveFeedback(@RequestBody FeedbackRequest request);
+
+    @GetMapping("/get")
+    List<Feedback> getFeedbacks();
+}
